aa2ab13ae088dfcd26f2ea82a461458f8c26e1cd
Daniel Conley <himself@danii.dev>
2021-12-03 15:23:37 -0500

    Rewrite data.py

--- dpy/data.py
+++ dpy/data.py
@@ -1,92 +1,208 @@
 from __future__ import annotations
+from typing_extensions import TypeAlias
 from .ducks import CacheManager
-from abc import ABC, abstractclassmethod
-from typing import Any, Optional, Union
+from abc import ABC, abstractmethod
+from itertools import tee
+from typing import Any, Callable, Generator, Generic, Literal, Optional, \
+	TypeVar, Union, cast, overload
 
-def entity(cls: type):
-	keys = cls.__dict__
-	annotations = cls.__annotations__.keys()
-	properties = [key for key in annotations if key not in keys]
+_T = TypeVar("_T")
+_I = TypeVar("_I")
+_constructorSelf = TypeVar("_constructorSelf", bound="_constructor[Any]")
 
-	def __init__(self: cls, key = None, /, **keywords):
-		super(cls, self).__init__(key, **keywords)
-		for key in properties:
-			the = key[:]
-			setattr(self, f"_{the}", keywords[the])
-	setattr(cls, "__init__", __init__)
+CacheFetchFetcher: TypeAlias = \
+	Callable[[CacheManager], Callable[[_I], Optional[_T]]]
 
-	for key in properties:
-		def get_getter(key):
-			def getter(self: cls):
-				return getattr(self, f"_{key}")
-			return getter
-		setattr(cls, key, property(get_getter(key)))
+class _constructor(ABC, Generic[_T]):
+	"""The abstract base class for defining self constructing properties.
 
-	return cls
+	Effectively a runtime controllable reference (one that can change where it
+	points to without changing itself) with information on how to build said
+	referenced data. The reference functionality is implemented with descriptors
+	and a magic property that must be defined on holding objects.
+	("__property_map__")
+	"""
 
-class Entity(ABC):
-	_INSTANTIATION_NONCE = object()
+	@abstractmethod
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> _T:
+		"""Constructs the referenced data."""
 
-	def __init__(self, key = None, /, **_):
-		if key != self._INSTANTIATION_NONCE:
-			raise TypeError("entities cannot be created manually")
+	@overload
+	def __get__(self: _constructorSelf, instance: None, owner: type) \
+			-> _constructorSelf: ...
+	@overload
+	def __get__(self, instance: Any, owner: type) -> _T: ...
+	def __get__(self: _constructorSelf, instance: Optional[Any], owner: type) \
+			-> Union[_constructorSelf, _T]:
+		"""Accesses the referenced data."""
 
-	@abstractclassmethod
-	def _from_api(cls, cache: Optional[CacheManager], data: Any): ...
+		if instance is None:
+			return self
 
-@entity
-class SelfUser(Entity):
-	id: int
-	username: str
+		property_map = getattr(instance, "__property_map__", None)
+		if property_map is None:
+			raise AttributeError("constructor cannot be used on values that do not have a __property_map__ field")
+		return property_map[self]()
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"],
-			username=data["username"])
+class _auto(_constructor[_T]):
+	"""Automagic constructor of inplace data.
 
-@entity
-class Guild(Entity):
-	id: int
+	Constructed data is type checked, and if the specified type is a decendant of
+	Entity, constructed.
+	"""
 
-	@property
-	def available(self):
-		return False
+	_Type: type[_T]
 
-	def __repr__(self) -> str:
-		return "UnavailableGuild"
+	def __init__(self, Type: type[_T]):
+		self._Type = Type
+		super().__init__()
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"])
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> Optional[_T]:
+		# If the specified type is a decendant of Entity...
+		if Entity in self._Type.mro():
+			# ...build the type.
+			return self._Type(data[property], cache)
+		else:
+			# Otherwise type check and return.
+			value = data.get(property, None)
+			if not isinstance(value, self._Type):
+				raise TypeError(f"expected type {self._Type}, found type {type(value)}")
+			return value
 
-@entity
-class AvailableGuild(Guild):
-	name: str
-	owner: int
+class _list_constructor(_constructor[_T]):
+	_constructor_: _constructor[_T]
 
-	roles: list[Role]
-	channels: list[GuildChannel]
+	def __init__(self, constructor: _constructor[_T]):
+		self._constructor_ = constructor
+		super().__init__()
 
-	@property
-	def available(self):
-		return True
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> list[_T]:
+		list_value: Union[list[Any], Any] = data[property]
+		if not isinstance(list_value, list):
+			raise TypeError()
 
-	def __repr__(self) -> str:
-		return self.name
+		list_return: list[_T] = []
+		for value in list_value:
+			construct = cast(_constructor[_T], self._constructor_).construct
+			list_return.append(construct(property, {property: value}))
+		return list_return
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		roles = [Role._from_api(cache, role) for role in data["roles"]]
-		channels = [GuildChannel._from_api(cache, channel) for channel in 
-			(data["channels"] if "channels" in data else [])]
+class _optional_constructor(_constructor[Optional[_T]]):
+	_constructor_: _constructor[_T]
 
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"], name=data["name"],
-			owner=data["owner_id"], roles=roles, channels=channels)
+	def __init__(self, constructor: _constructor[_T]):
+		self._constructor_ = constructor
+		super().__init__()
 
-@entity
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> Optional[_T]:
+		if property in data:
+			construct = cast(_constructor[_T], self._constructor_).construct
+			return construct(property, data, cache)
+		else:
+			return None
+
+class _convert_constructor(_constructor[_T], Generic[_I, _T]):
+	_constructor_: _constructor[_I]
+	_convert: Callable[[_I], _T]
+
+	def __init__(self, constructor: _constructor[_I],
+			convert: Callable[[_I], _T]):
+		self._constructor_ = constructor
+		self._convert = convert
+		super().__init__()
+
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> _T:
+		construct = cast(_constructor[_T], self._constructor_).construct
+		return self._convert(construct(property, data, cache))
+
+class _entity_reference(_constructor[Union[_T, _I]], Generic[_T, _I]):
+	def __init__(self, reference_to: type[_T],
+			fetch: Union[str, CacheFetchFetcher[_I, _T]], *,
+			id: _constructor[_I] = _convert_constructor(_auto(str), int)):
+		self._entity = reference_to
+		self._fetch = fetch
+		super().__init__()
+
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> _T:
+		if cache is None:
+			return data[property]
+		else:
+			fetcher = getattr(cache, self._fetch) if isinstance(self._fetch, str) \
+				else self._fetch(cache)
+			result = fetcher(data[property])
+
+			if result is not None and not isinstance(result, self._entity):
+				raise TypeError("expected entity")
+			return data[property] if result is None else result
+
+class _as(_constructor[_T]):
+	def __init__(self, constructor: _constructor[_T], as_: str):
+		self._constructor_ = constructor
+		self._as = as_
+		super().__init__()
+
+	def construct(self, property: str, data: dict[str, Any],
+			cache: Optional[CacheManager] = None) -> _T:
+		construct = cast(_constructor[_T], self._constructor_).construct
+		return construct(self._as, data, cache)
+
+class Entity:
+	"""An advanced tuple that can be built from raw JSON data.
+
+	All of an entities properties are "constructors", dynamic references which
+	encode information on how to build the data they point to. Regardless of this
+	systems complexity, it allows for easy creation of advanced data classes.
+	"""
+
+	__slots__ = "__property_map__", "_tuple"
+	__property_map__: dict[_constructor[Any], Callable[[], Any]]
+	_tuple: tuple[Any, ...]
+
+	def __init__(self, data: dict[str, Any], cache: Optional[CacheManager]=None):
+		def generate():
+			cls = type(self)
+			constructors = cast( # Python type checkers are dumb as ****.
+				Generator[tuple[str, _constructor[int]], None, None],
+				(
+					(key, property) for key in dir(cls)
+						if isinstance(property := getattr(cls, key), _constructor)
+				)
+			)
+
+			def get_getter(index: int):
+				return lambda: self._tuple[index]
+
+			for index, (key, property) in enumerate(constructors):
+				try:
+					value = property.construct(key, data, cache)
+				except Exception as exception:
+					raise ValueError(f"error occurred while constructing property \
+\"{key}\" of {type(self)}") from exception
+
+				yield (property, get_getter(index)), value
+
+		dict_generator, tuple_generator = tee(generate())
+		self.__property_map__ = dict(tuple for tuple, _ in dict_generator)
+		self._tuple = tuple(data for _, data in tuple_generator)
+
+_id_constructor = _convert_constructor(_auto(str), int)
+
+class User(Entity):
+	id = _id_constructor
+	username = _auto(str)
+
+class SelfUser(User):
+	pass
+
 class Role(Entity):
-	id: int
-	name: str
+	id = _id_constructor
+	name = _auto(str)
 
 	def __str__(self) -> str:
 		return f"<@&{self.id}>"
@@ -94,64 +210,55 @@ class Role(Entity):
 	def __repr__(self) -> str:
 		return self.name
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"], name=data["name"])
-
 class Channel(Entity):
-	id: int
+	id = _id_constructor
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		type = data["type"]
-
-		if type == 0:
-			return GuildTextChannel._from_api(cache, data)
-		elif type == 4:
-			return GuildCategoryChannel._from_api(cache, data)
-
-@entity
 class TextChannel(Channel):
 	pass
 
-@entity
 class GuildChannel(Channel):
-	name: str
+	name = _auto(str)
 
 	def __repr__(self) -> str:
 		return self.name
 
-@entity
 class GuildCategoryChannel(GuildChannel):
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"], name=data["name"])
+	pass
 
-@entity
 class GuildChildChannel(GuildChannel):
-	parent: Optional[Union[GuildCategoryChannel, int]]
+	parent = _optional_constructor(_entity_reference(GuildCategoryChannel, lambda c: c.get_channel)) #Optional[Union[GuildCategoryChannel, int]]
 
-@entity
 class GuildTextChannel(GuildChildChannel, TextChannel):
-	topic: str
-	nsfw: bool
+	topic = _auto(str)
+	nsfw = _auto(bool)
 
 	def __str__(self) -> str:
 		return f"<@#{self.id}>"
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		parent = data["parent_id"] if cache is None else \
-			cache.get_channel(data["parent_id"])
-
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"], name=data["name"],
-			topic=data["topic"], nsfw=data.get("nsfw", False), parent=parent)
-
-@entity
 class Message(Entity):
-	id: int
-	content: str
+	id = _auto(int)
+	content = _auto(str)
 
-	@classmethod
-	def _from_api(cls, cache: Optional[CacheManager], data):
-		return cls(cls._INSTANTIATION_NONCE, id=data["id"], content=data["content"])
+class Guild(Entity):
+	id = _id_constructor
+
+	@property
+	def available(self) -> bool:
+		return False
+
+	def __repr__(self) -> str:
+		return "UnavailableGuild"
+
+class AvailableGuild(Guild):
+	name = _auto(str)
+	owner = _as(_entity_reference(User, lambda c: c.get_user), "owner_id")
+
+	roles = _list_constructor(_auto(Role))
+	channels = _list_constructor(_auto(GuildChannel))
+
+	@property
+	def available(self) -> Literal[True]:
+		return True
+
+	def __repr__(self) -> str:
+		return self.name
--- dpy/gateway/__init__.py
+++ dpy/gateway/__init__.py
@@ -8,17 +8,22 @@ from json import dumps
 from typing import Awaitable, Callable, Optional
 
 class GatewayClient:
+	"""A client responsible for parsing and creating events from a gateway
+	connection.
+
+	
+	"""
 	token: str
-	dispatch: Callable[[Event], Awaitable]
+	dispatch: Callable[[Event], Awaitable[None]]
 	cache: Optional[CacheManager]
 
-	def __init__(self, dispatch: Callable[[Event], Awaitable],
-			cache: Optional[CacheManager] = None, token: str = None):
+	def __init__(self, dispatch: Callable[[Event], Awaitable[None]],
+			cache: Optional[CacheManager] = None, token: Optional[str] = None):
 		self.token = token
 		self.dispatch = dispatch
 		self.cache = cache
 
-	async def process_payload(self, manager: AsyncGatewayManager, payload):
+	async def process_payload(self, manager: AsyncGatewayManager, payload: dict[str, Any]):
 		op_code: int = payload["op"]
 		data: Any = payload["d"]
 
@@ -26,9 +31,9 @@ class GatewayClient:
 			event: str = payload["t"]
 
 			if event == "READY":
-				user = SelfUser._from_api(self.cache, data["user"])
-				guilds = [Guild._from_api(self.cache, guild) \
-					for guild in data["guilds"]]
+				print(data)
+				user = SelfUser(data["user"], self.cache)
+				guilds = [Guild(guild, self.cache) for guild in data["guilds"]]
 
 				if self.cache is not None:
 					for guild in guilds:
@@ -37,20 +42,20 @@ class GatewayClient:
 
 				await self.dispatch(ReadyEvent(user, guilds))
 			elif event == "GUILD_CREATE":
-				guild = AvailableGuild._from_api(self.cache, data)
+				guild = AvailableGuild(data, self.cache)
 
 				if self.cache is not None:
 					await self.cache.cache_guild(guild)
 
 				await self.dispatch(GuildCreateEvent(guild))
 			elif event == "MESSAGE_CREATE":
-				message = Message._from_api(self.cache, data)
+				message = Message(data, self.cache)
 
 				# TODO: CACHE
 
 				await self.dispatch(MessageCreateEvent(message))
 		elif op_code == 1:
-			await self.manager.heartbeat_now()
+			await manager.heartbeat_now()
 		elif op_code == 10: # Hello!
 			manager.heartbeat_interval = data["heartbeat_interval"]
 
